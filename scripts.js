54be70b81b987683b0b3a01534afc566248f74dd
Abdur-Rouf-01 <arouf10022@gmail.com>
2024-11-15 10:51:28 +0600

    Added Local Storage Saving System - For Last Formatted JSON

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let getInputJSON = document.getElementById('jsonInput');
 
 // Getting Data From the Input Field and Updating to Formatted JSON
 function formatJSON() {
-    let parseJSON = JSON.parse(getInputJSON.value);
+    let parseJSON = JSON.parse(getInputJSON.value) || localStorage.getItem('lastJSON');
     let parseToStringifyJSON = JSON.stringify(parseJSON, null, 4);
     getInputJSON.value = parseToStringifyJSON;
     //console.log(parseToStringifyJSON);
@@ -21,4 +21,19 @@ async function pasteText() {
 // Function For Clear Button
 function clearText() {
     getInputJSON.value = '';
+    localStorage.clear()
 }
+
+// Checking OnLoad if there any JSON in the Local Storage
+window.onload = () => {
+    if (localStorage !== null) {
+        formatJSON()
+    }
+    
+}
+
+// Adding Last JSON to Local Storage
+window.addEventListener('beforeunload', function () {
+    let parseJSON = JSON.parse(getInputJSON.value);
+    localStorage.setItem('lastJSON', parseJSON);
+})
